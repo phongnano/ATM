b99e98ea0483ebda3dcbfe9aba83cf6f1f0804f7
nguyenphong <nguyenphong@172.16.18.77>
2020-12-21 19:55:12 +0700

    done

--- ATMS.sql
+++ ATMS.sql
@@ -1,9 +1,3 @@
-create table LOGINS (
-    username    varchar(50) primary key ,
-    password    varchar(50)             ,
-    role        int                     
-);
-
 create table BANKS  (
     idbank      char(5)     primary key ,
     namebank    varchar(50)             
@@ -44,30 +38,36 @@ create table CUSTOMERS  (
     primary key (idcust, id)            
 );
 
-insert into BANKS
-values ('00000', 'Sacombank');
-insert into BANKS
-values ('00001', 'Vietcombank');
-insert into BANKS
-values ('00002', 'Techcombank');
-insert into BANKS
-values ('00003', 'TPBank');
-insert into BANKS
-values ('00004', 'VPBank');
+create table LOGINS (
+    username    varchar(50) unique ,
+    password    varchar(50)             ,
+    role        int                     ,
+    foreign key (username) references STAFFS (idstaff) ,
+    foreign key (username) references STAFFS (idcust)  
+);
 
-insert into LOGINS
-values ('admin', 'admin', 0);
-insert into LOGINS
-values ('phong', 'phong', 1);
-insert into LOGINS
-values ('user', 'user', 2);
-
-insert into STAFFS
-values ('025786236', '0123456', 'Nguyễn Hoàng Long', '1998-03-24', 0, 'TP.HCM', '0589884191', '24031998', 1);
-
-insert into CUSTOMERS
-values ('025786236', '0123456', 'Nguyễn Hoàng Phong', '1998-03-24', 0, 'TP.HCM', '0589884191', '24031998', 2);
-
-select * from BANKS
-=======
-select * from CUSTOMERS
+-- insert into BANKS
+-- values ('00000', 'Sacombank');
+-- insert into BANKS
+-- values ('00001', 'Vietcombank');
+-- insert into BANKS
+-- values ('00002', 'Techcombank');
+-- insert into BANKS
+-- values ('00003', 'TPBank');
+-- insert into BANKS
+-- values ('00004', 'VPBank');
+-- 
+-- insert into LOGINS
+-- values ('admin', 'admin', 0);
+-- insert into LOGINS
+-- values ('phong', 'phong', 1);
+-- insert into LOGINS
+-- values ('user', 'user', 2);
+-- 
+-- insert into STAFFS
+-- values ('025786236', '0123456', 'Nguyễn Hoàng Long', '1998-03-24', 0, 'TP.HCM', '0589884191', '24031998', 1);
+-- 
+-- insert into CUSTOMERS
+-- values ('025786236', '0123456', 'Nguyễn Hoàng Phong', '1998-03-24', 0, 'TP.HCM', '0589884191', '24031998', 2);
+-- 
+-- select * from BANKS
