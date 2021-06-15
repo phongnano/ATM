16d41054cc786f6e671402ae6d02429b38b7af80
phongnano <70794031+phongnano@users.noreply.github.com>
2021-06-15 23:10:40 +0700

    done

--- src/Transactions/BLL_Transactions.java
+++ src/Transactions/BLL_Transactions.java
@@ -4,11 +4,11 @@ public class BLL_Transactions {
 
     DAL_Transactions dal = new DAL_Transactions();
 
-    public int Deposit(int amount, String usr) {
+    public int Deposit(long amount, String usr) {
         return dal.Deposit(amount, usr);
     }
 
-    public int Withdraw(int amount, String usr) {
+    public int Withdraw(long amount, String usr) {
         return dal.Withdraw(amount, usr);
     }
 }
